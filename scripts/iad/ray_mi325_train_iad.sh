4aa3d2d5ec079f83bb873f2e3a11c63ce7a76e41
XavierZXY <zxy_xavier@163.com>
2025-10-25 06:01:09 +0000

    refactor(training): streamline training script by consolidating configuration parameters for better maintainability and clarity

---
+++ scripts/iad/ray_mi325_train_iad.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+set -x
+export VLLM_USE_V1=1
+# export VERL_LOGGING_LEVEL=INFO
+export LLM_AS_A_JUDGE_BASE="http://tw034:9091/v1"
+# load key from text file, the file is in the same directory as this script
+export WANDB_API_KEY=$(cat scripts/iad/wandb_key)
+export SWANLAB_API_KEY=$(cat scripts/iad/swanlab_key)
+PROJECT_NAME="iad-tool"
+
+BASEDIR=/wekafs/takisobe/zxy/codes/verl
+SAVE_CHECKPOINT_DIR=/wekafs/takisobe/zxy/models/verl_checkpoints
+# DATASET_TRAIN=${BASEDIR}/data/train.parquet
+# DATASET_VAL=${BASEDIR}/data/val.parquet
+# DATASET_TRAIN=/wekafs/takisobe/zxy/data/deepeyes/output_first_300.parquet
+# DATASET_VAL=/wekafs/takisobe/zxy/data/deepeyes/output_first_300.parquet
+# DATASET_TRAIN=/wekafs/takisobe/zxy/codes/verl/data/visa-2k/train/train.parquet
+# DATASET_TRAIN=/wekafs/takisobe/zxy/codes/verl/data/mvtec/good/compare_output.parquet
+# DATASET_TRAIN=/wekafs/takisobe/zxy/codes/verl/data/mvtec/train/train_enhanced.parquet
+# DATASET_VAL=/wekafs/takisobe/zxy/codes/verl/data/visa-2k/test/test.parquet
+# debug for new data
+DATASET_TRAIN=/wekafs/takisobe/zxy/datasets/qwen-iad/train/train.parquet
+DATASET_VAL=/wekafs/takisobe/zxy/datasets/qwen-iad/test/test.parquet
+REF_MODEL_PATH=/wekafs/takisobe/zxy/models/Qwen2.5-VL-7B-Instruct
+# ---------------- Train config -----------------
+WORLD_SIZE=2
+TOTAL_EPOCHS=5
+BATCH_SIZE=32
+PPO_BATCH_SIZE=32
+MICRO_BATCH_SIZE=4
+LR=1e-6
+LOG_PER_GPU_BATCH_SIZE=8
+ROLLOUT_PARALLELISM=1
+ROLLOUT_UTIL=0.5
+N_GPUS_PER_NODE=8
+N_ROLLOUT=8
+GRAD_CLIP=1
+SAVE_FREQ=20
+TEST_FREQ=10
+# EXPERIMENT_NAME="TW-003-Tool-Mvtex-train-val-3B-lr${LR}-grad-clip${GRAD_CLIP}-batch${BATCH_SIZE}-ppo${PPO_BATCH_SIZE}-micro${MICRO_BATCH_SIZE}-grpo"
+EXPERIMENT_NAME="Multizoom-rollout${N_ROLLOUT}-batch${BATCH_SIZE}-ppo${PPO_BATCH_SIZE}-micro${MICRO_BATCH_SIZE}"
+
+# ---------------- Train config -----------------
+ray job submit --address="10.32.80.105:6379" \
+--working-dir=${BASEDIR} \
+--no-wait \
+-- \
+PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo \
+    --config-path=${BASEDIR}/recipe/qwen_iad/configs \
+    --config-name='qiad_multiturn_grpo' \
+    data.train_files=${DATASET_TRAIN} \
+    data.val_files=[${DATASET_VAL}] \
+    data.train_batch_size=${BATCH_SIZE} \
+    data.max_prompt_length=8192 \
+    data.max_response_length=16384 \
+    data.return_raw_chat=True \
+    data.filter_overlong_prompts=True \
+    algorithm.adv_estimator=grpo \
+    algorithm.kl_ctrl.kl_coef=0.0 \
+    actor_rollout_ref.model.path=${REF_MODEL_PATH} \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.model.use_fused_kernels=False \
+    actor_rollout_ref.actor.optim.lr=${LR} \
+    actor_rollout_ref.actor.ppo_mini_batch_size=${PPO_BATCH_SIZE} \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=${MICRO_BATCH_SIZE} \
+    actor_rollout_ref.actor.use_kl_loss=False \
+    actor_rollout_ref.actor.kl_loss_coef=0.0 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.actor.entropy_coeff=0.0 \
+    actor_rollout_ref.actor.grad_clip=${GRAD_CLIP} \
+    actor_rollout_ref.actor.checkpoint.save_contents=['model','hf_model','optimizer','extra'] \
+    actor_rollout_ref.actor.ulysses_sequence_parallel_size=1 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=${LOG_PER_GPU_BATCH_SIZE} \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=${ROLLOUT_PARALLELISM} \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.mode=async \
+    actor_rollout_ref.rollout.n=${N_ROLLOUT} \
+    actor_rollout_ref.rollout.max_num_batched_tokens=32768 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=${ROLLOUT_UTIL} \
+    actor_rollout_ref.rollout.enforce_eager=False \
+    actor_rollout_ref.rollout.free_cache_engine=True \
+    actor_rollout_ref.rollout.enable_chunked_prefill=True \
+    actor_rollout_ref.rollout.enable_prefix_caching=False \
+    actor_rollout_ref.model.use_shm=False \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=${LOG_PER_GPU_BATCH_SIZE} \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    actor_rollout_ref.rollout.multi_turn.enable=True \
+    actor_rollout_ref.rollout.multi_turn.max_assistant_turns=4 \
+    actor_rollout_ref.rollout.multi_turn.max_user_turns=4 \
+    actor_rollout_ref.rollout.multi_turn.max_parallel_calls=1 \
+    actor_rollout_ref.rollout.multi_turn.tool_config_path=recipe/qwen_iad/configs/image_tools_config.yaml \
+    trainer.critic_warmup=0 \
+    trainer.logger=['wandb','swanlab','console'] \
+    trainer.val_before_train=True \
+    trainer.n_gpus_per_node=${N_GPUS_PER_NODE} \
+    trainer.nnodes=${WORLD_SIZE} \
+    trainer.save_freq=${SAVE_FREQ} \
+    trainer.test_freq=${TEST_FREQ} \
+    trainer.validation_data_dir=${BASEDIR}/logs/validation_data/${EXPERIMENT_NAME} \
+    trainer.project_name=${PROJECT_NAME} \
+    trainer.experiment_name=${EXPERIMENT_NAME} \
+    trainer.default_local_dir=${SAVE_CHECKPOINT_DIR}/${PROJECT_NAME}/${EXPERIMENT_NAME} \
+    +trainer.tensorboard_dir=${SAVE_CHECKPOINT_DIR}/logs/tensorboard \
+    +trainer.rl_logging_board_dir=${SAVE_CHECKPOINT_DIR}/logs/rl_logging_board \
+    trainer.rollout_data_dir=${BASEDIR}/logs/rollout_data/${EXPERIMENT_NAME} \
+    trainer.total_epochs=${TOTAL_EPOCHS} 2>&1 | tee ./logs/${EXPERIMENT_NAME}.log
