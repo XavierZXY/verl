dc80d20863277acb32b079a8e1d0b7aa926e008e
XavierZXY <zxy_xavier@163.com>
2025-10-26 15:35:26 +0000

    refactor(training): change project name from 'iad-tool' to 'iad-zoom' for improved clarity in training configuration

--- scripts/iad/mi325_train_iad.sh
+++ scripts/iad/mi325_train_iad.sh
@@ -7,7 +7,7 @@ export LLM_AS_A_JUDGE_BASE="http://tw034:9091/v1"
 # load key from text file, the file is in the same directory as this script
 export WANDB_API_KEY=$(cat scripts/iad/wandb_key)
 export SWANLAB_API_KEY=$(cat scripts/iad/swanlab_key)
-PROJECT_NAME="iad-tool"
+PROJECT_NAME="iad-zoom"
 
 BASEDIR=/wekafs/takisobe/zxy/codes/verl
 SAVE_CHECKPOINT_DIR=/wekafs/takisobe/zxy/models/verl_checkpoints
